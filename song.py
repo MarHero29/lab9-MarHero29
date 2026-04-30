bc17a52769ddf5c257baec61f4ba4bc6dd634770
Mariana Hernandez <marianahero@unisabana.edu.co>
2026-04-30 11:04:43 -0500

    1

--- song.py
+++ song.py
@@ -2,7 +2,7 @@ class Song:
     def __init__(self, name, artist, length):
         self.name = name
         self.artist = artist
-        self.length = length  # en minutes
+        self.length = length
 
     def get_length_in_seconds(self):
         return self.length * 60
